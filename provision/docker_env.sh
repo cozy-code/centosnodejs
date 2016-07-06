ae307d75ff204dcf04220fb1861ecb17154411f0
cozy code <cozy.code@outlook.com>
2016-07-06 09:18:28 +0900

    change docker permission

--- provision/docker_env.sh
+++ provision/docker_env.sh
@@ -8,13 +8,14 @@ if ! ( yum list installed | grep ^docker > /dev/null ); then
     sudo rpm -ivh http://ftp.jaist.ac.jp/pub/Linux/Fedora/epel/6/i386/epel-release-6-8.noarch.rpm
     sudo yum -y install docker-io
 
+    # enable docker for normal user
+    sudo groupadd docker
+    sudo usermod -g docker $USER
+
     #start as service
     sudo /etc/init.d/docker start
     sudo chkconfig docker on
 
-    # enable docker for normal user
-    sudo chown root:dockerroot /var/run/docker.sock
-    sudo usermod -aG dockerroot $USER
 fi
 
 
